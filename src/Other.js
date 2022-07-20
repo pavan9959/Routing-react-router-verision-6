e04f43b5c45913f93167652c24007da8d1abd296
pavanCronj <pavan@cronj.com>
2022-07-20 16:51:21 +0530

    updates

--- src/Other.js
+++ src/Other.js
@@ -3,10 +3,12 @@ import { ReactDOM } from "react";
 import { Link, Route } from "react-router-dom";
 import { useDispatch,useSelector } from "react-redux";
 import { Plus } from "./Action";
+import { useNavigate } from "react-router-dom";
 
 function Other() {
 
   const dispatch=useDispatch()
+  const nav=useNavigate()
 
   const handelChange=()=>{
     dispatch(Plus())
@@ -16,11 +18,16 @@ function Other() {
     return stat.c
   })
 
+  const handleClick=()=>{
+    nav("/")
+  }
+
   return (
     <div>
       <h1>Count</h1>
       <button onClick={()=>{handelChange()}} >plus</button>
       <p>{data}</p>
+      <button onClick={handleClick} >Go to app using usenavigate hook</button>
     </div>
   );
 }
